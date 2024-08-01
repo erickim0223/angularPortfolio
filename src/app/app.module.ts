71c82f24e47a819cc1e1bd830992a7c69e6bb782
erick <erick@LAPTOP-4ABGE1VI>
2024-07-31 22:02:53 -0400

    changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
